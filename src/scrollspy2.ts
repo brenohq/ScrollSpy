76d44518408e7c36d53a2ac8e79503e712837642
siminino <victor.areas@corp.globo.com>
2020-08-14 12:38:30 -0300

    implementing ScrollSpy using Intersection Observer

--- src/scrollspy2.ts
+++ src/scrollspy2.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2016, Globo.com (https://github.com/globocom)
+ *
+ * License: MIT
+ */
+
 export interface ScrollSpyItemOptions {
   el: HTMLElement;
   callback: () => void;
@@ -10,81 +16,42 @@ export type ScrollSpyItemReference = "top" | "bottom";
 export interface ScrollSpyItem extends ScrollSpyItemOptions {
   offset: number;
   reference: ScrollSpyItemReference;
-  pos: number;
+}
+
+export interface IntersectionObserver {
+    rootMargin: string,
+    threshold: number
 }
 
 let items: ScrollSpyItem[] = [];
-
-// ---------
 
 export function clean(): void {
   items = [];
 }
 
-function getItems(): readonly Readonly<ScrollSpyItem>[]{
+export function getItems(): readonly Readonly<ScrollSpyItem>[]{
   return items.map(i => i);
 }
 
-// function add(param: ScrollSpyItemOptions): void {
-//     if (!param.el) {
-//         throw new Error("[@globocom/scrollspy] item.el is required");
-//     }
-//     const item: ScrollSpyItem = Object.assign(
-//         { offset: 200, reference: "top", pos: 0 },
-//         param
-//     );
-//     item.pos = getElementPos(item);
-
-//     const index = items.findIndex(i => i.pos > item.pos);
-
-//     items.splice(index === -1 ? items.length : index, 0, item);
-
-//     if (items.length === 1) {
-//     setDefaultVariables();
-//     startListener();
-//     }
-//     checkVisibleItems();
-// }
-
-interface IntersectionObserver {
-    rootMargin: string,
-    threshold: number
-}
-
-function add(param: ScrollSpyItemOptions): void {
+export function add(param: ScrollSpyItemOptions): void {
   if (!param.el) {
     throw new Error("[@globocom/scrollspy] item.el is required");
   }
-  const item: ScrollSpyItem = Object.assign(
-    { offset: 200, reference: "top", pos: 0 },
-    param
-  );
 
-  let rootMargin = "";
-  if (item.reference === "top") {
-    // rootMargin = `${item.offset}px 0px 0px 0px`;
-    rootMargin = `${item.offset}px 0px ${item.offset}px 0px`;
-  } else {
-    // rootMargin = `0px 0px ${item.offset}px 0px`;
-    
-  }
+  const item: ScrollSpyItem = { offset: 200, reference: "top", ...param };
+  items.push(item);
 
-  let options = {
-    rootMargin: rootMargin,
-    threshold: 0.01
-  }
-
-  let observer = new IntersectionObserver(param.callback, options);
-  observer.observe(item.el);
+  observeItem(item, param.callback);
 }
 
-function debug() {
+export function debug() {
   items.forEach((item, i) => {
     const color = i % 2 ? "red" : "blue";
     const border = `2px dashed ${color}`;
     const nodeHtml = document.createElement("div");
+    const position = getElementPos(item);
     const css = [
-      `top: ${item.pos};`,
+      `top: ${position};`,
       "width: 100%;",
       "position: absolute;",
       `border-top: ${border};`
@@ -97,4 +64,27 @@ function debug() {
     document.body.appendChild(nodeHtml);
   });
   return items;
+}
+
+function observeItem(item: ScrollSpyItem, callback: () => void): void {
+  let rootMargin = "";
+  if (item.reference === "top") {
+    rootMargin = `${item.offset}px 0px ${item.offset}px 0px`;
+  } else {
+    let offset = -1 * (((item.el.offsetHeight / 100) * 99) - item.offset);
+    rootMargin = `0px 0px ${offset}px 0px`;
+  }
+
+  let options = {
+    rootMargin: rootMargin,
+    threshold: 0.01
+  };
+
+  let observer = new IntersectionObserver(callback, options);
+  observer.observe(item.el);
+}
+
+function getElementPos(item: ScrollSpyItem): number {
+  const boundClient = item.el.getBoundingClientRect();
+  return boundClient[item.reference] - item.offset;
 }
